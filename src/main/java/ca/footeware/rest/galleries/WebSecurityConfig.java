82802bed7f9930c3c998678c4043090a273f2e68
Craig Foote <CraigFoote@gmail.com>
2023-01-18 15:40:57 -0500

    cors

---
+++ src/main/java/ca/footeware/rest/galleries/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package ca.footeware.rest.galleries;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Specify the URLs to which the current user can access and those that are
+ * forwarded to login.
+ *
+ * @author Footeware.ca
+ */
+@Configuration
+public class WebSecurityConfig {
+
+	/**
+	 * A bean that configures HTTP security.
+	 *
+	 * @param {@link HttpSecurity}
+	 * @return {@link SecurityFilterChain}
+	 * @throws Exception when the internet falls over.
+	 */
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.cors().and().csrf().disable().build();
+	}
+
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/recipes/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS",
+						"DELETE", "PUT");
+			}
+		};
+	}
+}
